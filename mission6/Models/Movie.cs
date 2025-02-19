b504b9890cc36f46813ab131fada6b32fcdd294b
christiaylor <tay10r@byu.edu>
2025-02-19 09:08:36 -0700

    Added db and mostly fixed movie.cs

--- mission6/Models/Movie.cs
+++ mission6/Models/Movie.cs
@@ -8,22 +8,26 @@ namespace mission6.Models
         [Key]
         [Required]
         public int Id { get; set; }
-        [Required]
+
         public int Category { get; set; }
+
         [Required]
         public string Title { get; set; }
+
         [Required]
         public int Year { get; set; }
-        [Required]
+
         public string Director { get; set; }
-        [Required]
+
         public string Rating { get; set; }
 
-        // Non Required fields
+        [Required]
         public bool? Edited { get; set; }
-        public string? LentTo { get; set; }
-        public string? CopiedToPlex { get; set; }
 
+        public string? LentTo { get; set; }
+
+        [Required]
+        public string? CopiedToPlex { get; set; }
 
         [MaxLength(25)]
         public string? Notes { get; set; }
